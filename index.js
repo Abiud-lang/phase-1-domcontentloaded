115d7ecf97e623b40d16df0a1933cd4f708ba4b7
Abiud-lang <abiud.kiprotich@student.moringaschool.com>
2024-07-07 23:19:25 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    const p = document.querySelector('p');
+    if (p) {
+      p.textContent = 'This is really cool!';
+    }
+  });
